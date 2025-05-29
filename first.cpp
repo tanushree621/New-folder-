dd3cf6bd74150cac195a73794000da3d1ea25e24
Tanushree Mahajan <tanushreemahajan333@gmail.com>
2025-05-29 10:32:09 +0530

    first commit

---
+++ first.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int a =-152 ;
+   cout << "Hello guys " << a << endl;
+   bool b = true;
+   cout << b << endl;
+
+   float f = 1.32;
+   cout << f << endl;
+
+   int size = sizeof(a);
+   cout << "Size of int "<< size << endl;
+
+   char ch = 56;
+   cout << ch << endl; 
+
+    unsigned h = 46265;
+    cout<< h << endl; 
+    int d= 2/4;
+    cout << d<<endl;
+}  
